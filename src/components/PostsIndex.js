eb637352f643f85ba514322a711b8bb198f3946d
Nazar Lesiv <nazarlesiv@Nazars-MBP.home>
2016-11-25 16:59:15 -0500

    add redux-form

--- src/components/PostsIndex.js
+++ src/components/PostsIndex.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
 
-export default () => {
-  return (
-    <div>List of Blog Posts</div>
-  );
+class PostsIndex extends Component {
+  render() {
+    return (
+      <div>
+        <div className='text-xs-right'>
+          <Link to='/posts/new' className='btn btn-primary'>
+            Add a Post
+          </Link>
+        </div>
+        List of Blog Posts
+      </div>
+    );
+  }
+  componentWillMount() {
+    console.log('componenetWillMount');
+    // Get the posts.
+    this.props.fetchPosts();
+  }
 }
+
+/*
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchPosts }, dispatch);
+}
+*/
+
+function mapStateToProps() {
+  return {
+    posts: this.props.posts
+  };
+}
+//export default PostsIndex;
+export default connect(null, { fetchPosts })(PostsIndex);
